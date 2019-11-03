59b25ac1bb06521a0ed1981bfed2c39ef0291411
sharbel93 <sharbel@techfactory.ltd>
2019-11-03 16:52:04 +0300

    dealing with Lists of Elements

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -4,6 +4,7 @@ import {Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output} from '@angular
 import {HeroService} from '../hero.service';
 import {of} from 'rxjs/internal/observable/of';
 import {Hero} from '../hero';
+import {By} from '@angular/platform-browser';
 
 describe('HeroesComponent (shallow tests)', () => {
  let fixture: ComponentFixture<HeroesComponent>;
@@ -39,4 +40,9 @@ describe('HeroesComponent (shallow tests)', () => {
    fixture.detectChanges();
    expect(fixture.componentInstance.heroes.length).toBe(3);
  });
+  it('should create one li for each hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+  });
 });
