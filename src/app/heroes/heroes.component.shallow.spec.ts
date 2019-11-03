ea9cbef47830fedc7f01897fb1c4aaab838cd25f
sharbel93 <sharbel@techfactory.ltd>
2019-11-03 16:36:06 +0300

    mocking child components

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,14 +1,24 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeroesComponent} from './heroes.component';
-import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output} from '@angular/core';
 import {HeroService} from '../hero.service';
 import {of} from 'rxjs/internal/observable/of';
+import {Hero} from '../hero';
 
 describe('HeroesComponent (shallow tests)', () => {
  let fixture: ComponentFixture<HeroesComponent>;
  let mockHeroService;
  let HEROES;
- beforeEach(() => {
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
+    beforeEach(() => {
    HEROES = [
      {id: 1, name: 'SpiderDude', strength: 8},
      {id: 2, name: 'Wonderful Woman', strength: 24},
@@ -16,11 +26,11 @@ describe('HeroesComponent (shallow tests)', () => {
    ];
    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
    TestBed.configureTestingModule({
-     declarations: [HeroesComponent],
+     declarations: [HeroesComponent, FakeHeroComponent],
      providers: [
        {provide: HeroService, useValue: mockHeroService }
        ],
-     schemas: [NO_ERRORS_SCHEMA]
+     // schemas: [NO_ERRORS_SCHEMA]
    });
    fixture = TestBed.createComponent(HeroesComponent);
  });
