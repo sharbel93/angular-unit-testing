5def12fd5fa36f36538737a6adbfc6b722e36c55
sharbel93 <sharbel@techfactory.ltd>
2019-11-05 08:10:59 +0300

    Testing DOM Interaction and routing Components - Testing with ActivatedRoute

---
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,26 @@
+import {TestBed} from '@angular/core/testing';
+import {HeroDetailComponent} from './hero-detail.component';
+import {ActivatedRoute} from '@angular/router';
+import {HeroService} from '../hero.service';
+import {Location} from '@angular/common';
+
+describe('HeroDetailComponent', () => {
+  let fixture, mockActivatedRoute, mockHeroService, mockLocation;
+  beforeEach(() => {
+    mockActivatedRoute = {
+      snapshot: {paramMap: { get: () => '3'}}
+    };
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+    mockLocation = jasmine.createSpyObj(['back']);
+    //
+    TestBed.configureTestingModule({
+      declarations: [HeroDetailComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: HeroService, useValue: mockHeroService },
+        { provide: Location, useValue: mockLocation }
+      ]
+    });
+    fixture = TestBed.createComponent(HeroDetailComponent);
+  });
+});
