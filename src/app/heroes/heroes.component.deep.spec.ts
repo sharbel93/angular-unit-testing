cf63f92dcad80f961fd5234261cab08a636c8f4e
sharbel93 <sharbel@techfactory.ltd>
2019-11-05 08:41:23 +0300

    Testing DOM Interaction and routing Components - Testing the RouterLink

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -80,4 +80,16 @@ describe('HeroesComponent (deep tests)', () => {
     const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
     expect(heroText).toContain(name);
   });
+  //
+  it ('should have the correct route for the first hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    //
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    let routerLink = heroComponents[0].query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+    //
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+    expect(routerLink.navigatedTo).toBe('/detail/1');
+  });
 });
